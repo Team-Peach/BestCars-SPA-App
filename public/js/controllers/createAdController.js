3a3b774ff5221ac63098293e1b6f1a9b194d536c
Georgi <g.borisov123@gmail.com>
2017-04-29 23:29:40 +0300

    createAd template and createAd controller

--- public/js/controllers/createAdController.js
+++ public/js/controllers/createAdController.js
@@ -6,36 +6,21 @@ import { createAd as createAd } from 'data';
 export function createAdController(context) {
 	loadTemplate('createAd')
 		.then(template => {
-			context.$element().html(template);
+			context.$element().html(template());
 
-			// let $loginForm = $('#createAd-form');
-			// $loginForm.on('submit', function () {
-			// 	let aaa = $('#createAd-town option:selected').text();
-			// 	let bbb = $('#createAd-town1 option:selected').text();
-			// 	let aaaa = $('#createAd-town2 option:selected').text();
-			// 	let ddd = $('#createAd-town3 option:selected').text();
+			$('#form-createAd').submit(function () {
+				let arrayOfFormObjValues = $('#form-createAd').serializeArray();
 
-			// 	let params = {};
+				let valuesFromForm = {};
+				$.each(arrayOfFormObjValues, function (i, field) {
+					valuesFromForm[field.name] = field.value;
+				});
 
-			// 	// TODO get all drop down
-			// 	if (params.hasOwnProperty()) {
+				let typeOfVehicleAd = valuesFromForm.type;
 
-			// 	}
+				let authtoken = sessionStorage.getItem('authtoken');
 
-			// 	let type = 'cars';
-
-			// 	params = {
-			// 		aaa,
-			// 		bbb,
-			// 		aaaa,
-			// 		ddd
-			// 	};
-
-			// 	let authtoken = sessionStorage.getItem('authtoken');
-
-			// 	createAd(params, authtoken, type);
-			// });
+				createAd(valuesFromForm, authtoken, typeOfVehicleAd);
+			});
 		});
 }
-
-
