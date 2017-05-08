c848e7388fc2dcd9795bf6410deddb69bc11e0e8
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-05-08 22:16:05 +0300

    Refactoring in tests

--- public/tests/data-tests.js
+++ public/tests/data-tests.js
@@ -1,6 +1,18 @@
 import * as data from 'data'
+
+mocha.setup('bdd');
+let expect = chai.expect;
+
+//data tests
 describe('Data Tests', () => {
     it('Test 1', () => {
+    })
+})
+
+//user Controller test
+describe('User Controller Tests', () => {
+    it('Test 1', () => {
+
     })
 })
 
