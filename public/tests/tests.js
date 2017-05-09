e0b4fe55c7c0900b05f709aff8a01a31905c0283
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-05-09 15:34:25 +0300

    Added test for getVehicles, getMyAds, createAd function from data.js

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -2,6 +2,7 @@
 import * as requester from 'requester';
 import * as data from 'data';
 import { User } from 'user';
+import { Car } from 'car';
 import * as CONSTANTS from 'constants';
 
 mocha.setup('bdd');
@@ -98,8 +99,8 @@ describe('Data Tests', () => {
             }
             let password = '123456';
             let response = {
-                "username": "user", 
-                "password": "123456"
+                "username": "user",
+                "authkey": "authkey123"
             }
 
             requesterPostStub.returns(Promise.resolve(response));
@@ -111,34 +112,11 @@ describe('Data Tests', () => {
                 })
                 .then(done, done);
         })
-
-        it('(6) expect registerUser to call post function from requester with correct user data(password)', (done) => {
-
-            let url = CONSTANTS.kinveyRegisterUserUrl;
-            let user = {
-                "username": "user"
-            }
-            let password = '123456';
-            let response = {
-                "username": "user", 
-                "password": "123456"
-            }
-
-            requesterPostStub.returns(Promise.resolve(response));
-
-            data.registerUser(user, password)
-                .then(() => {
-                    let responseUsername = JSON.parse(requesterPostStub.args[0][1]).password;
-                    console.log(response)
-                    expect(responseUsername).to.equal(password);
-                })
-                .then(done, done);
-        })     
     })
 
-    // TODO login
+    // login
 
-        describe('Data.js loginUser', () => {
+    describe('Data.js loginUser', () => {
         let requesterPostStub;
         beforeEach(() => {
             requesterPostStub = sinon.stub(requester, 'post');
@@ -147,11 +125,11 @@ describe('Data Tests', () => {
             requesterPostStub.restore();
         });
 
-        it('(7) expect loginUser to call post function from requester once', (done) => {
+        it('(6) expect loginUser to call post function from requester once', (done) => {
             let user = { 'username': "user", 'password': "123456" };
-            
+
             requesterPostStub.returns(Promise.resolve());
- 
+
             data.loginUser(user)
                 .then(() => {
                     expect(requesterPostStub).to.have.been.called.once;
@@ -159,12 +137,12 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(8) expect loginUser to call post function with correct url', (done) => {
+        it('(7) expect loginUser to call post function with correct url', (done) => {
 
             requesterPostStub.returns(Promise.resolve());
             let url = CONSTANTS.kinveyRegisterUserUrl + '/login'
             let user = { 'username': "user", 'password': "123456" };
-  
+
             data.loginUser(user)
                 .then(() => {
                     expect(requesterPostStub).to.have.been.calledWith(url);
@@ -172,12 +150,12 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(9) expect loginUser to call post function from requester with correct user data(username)', (done) => {
-
-            requesterPostStub.returns(Promise.resolve());
+        it('(8) expect loginUser to call post function from requester with correct user data(username)', (done) => {
 
             let url = CONSTANTS.kinveyRegisterUserUrl + '/login'
             let user = { 'username': "user", 'password': "123456" };
+            let response = { 'username': "user", 'authkey': "authkey123" };
+            requesterPostStub.returns(Promise.resolve(response));
 
             data.loginUser(user)
                 .then(() => {
@@ -187,7 +165,7 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(10) expect loginUser to call post function from requester with correct user data(password)', (done) => {
+        it('(9) expect loginUser to call post function from requester with correct user data(password)', (done) => {
 
             requesterPostStub.returns(Promise.resolve());
 
@@ -200,7 +178,7 @@ describe('Data Tests', () => {
                     expect(responseUsername).to.equal(user.password);
                 })
                 .then(done, done);
-        })     
+        })
     })
 
     //logout function
@@ -214,10 +192,10 @@ describe('Data Tests', () => {
             requesterPostStub.restore();
         });
 
-        it('(11) expect logoutUser to call post function from requester once', (done) => {
-            
+        it('(10) expect logoutUser to call post function from requester once', (done) => {
+
             requesterPostStub.returns(Promise.resolve());
- 
+
             data.logoutUser()
                 .then(() => {
                     expect(requesterPostStub).to.have.been.called.once;
@@ -225,30 +203,271 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(12) expect logoutUser to call post function with correct url', (done) => {
+        it('(11) expect logoutUser to call post function with correct url', (done) => {
             let url = CONSTANTS.kinveyRegisterUserUrl + '/_logout';
             requesterPostStub.returns(Promise.resolve());
-  
+
             data.logoutUser()
                 .then(() => {
                     expect(requesterPostStub).to.have.been.calledWith(url);
                 })
                 .then(done, done);
-        }) 
+        })
     })
 
+    //getVehicles
+    describe('Data.js getVehicles', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'get');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
 
+        it('(12) expect getVehicles to call post function from requester once', (done) => {
+            let collection = '/cars';
+            requesterPostStub.returns(Promise.resolve());
 
-//TOD0 deleteVehicle 
+            data.getVehicles(collection)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
 
-// TODO getVehicles
+        it('(13) expect getVehicles to call post function with correct url', (done) => {
+            let collection = '/cars';
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + collection + '/' + '?query={}&sort={"_kmd": -1}';
+            requesterPostStub.returns(Promise.resolve());
 
-//getMyAds
-// getUserProfileByAd
+            data.getVehicles(collection)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        })
+    })
+    // getMyAds
 
-//createAd
+    describe('Data.js getMyAds', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'get');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
 
-// add UserProfileImage
+        it('(14) expect getMyAds to call post function from requester once', (done) => {
+            let creatorId = 'authorId';
+            let collection = '/cars';
+            let authtoken = "auth123";
+            requesterPostStub.returns(Promise.resolve());
+
+            data.getMyAds(creatorId, collection, authtoken)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
+
+        it('(15) expect getMyAds to call post function with correct url', (done) => {
+            let creatorId = 'authorId';
+            let collection = '/cars';
+            let authtoken = "auth123";
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + collection + '/' + '?query={"_acl.creator":"' + creatorId + '"}';
+            requesterPostStub.returns(Promise.resolve());
+
+            data.getMyAds(creatorId, collection, authtoken)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        })
+    })
+
+    //createAd
+    describe('Data,js - createAd', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'post');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
+
+        // with instance of new Car
+        it('(16) expect createAd to call post function from requester once', (done) => {
+            requesterPostStub.returns(Promise.resolve());
+            let carStub = sinon.createStubInstance(Car);
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+            data.createAd(carStub, authtoken, type)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
+
+        it('(17) expect createAd to call post function with correct url', (done) => {
+
+            requesterPostStub.returns(Promise.resolve());
+            let carStub = sinon.createStubInstance(Car);
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + type;
+
+            data.createAd(carStub, authtoken, type)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        })
+
+        // with  object car with correct properties
+        it('(18) expect createAd to call post function from requester once', (done) => {
+            //currentVehicle = factory.createCar(valuesFromForm.manufacturer, valuesFromForm.model, valuesFromForm.year, valuesFromForm.kilometers, valuesFromForm.price, valuesFromForm.fuel, valuesFromForm.transmission, valuesFromForm.horsepower, valuesFromForm.type, valuesFromForm.numberOfDoors, valuesFromForm.numberOfSeats);
+            requesterPostStub.returns(Promise.resolve());
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+
+            let car = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+            data.createAd(car, authtoken, type)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
+
+        it('(19) expect createAd to make a POST request with correct url', (done) => {
+
+            requesterPostStub.returns(Promise.resolve());
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + type;
+
+            let car = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+            data.createAd(car, authtoken, type)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        })
+
+        it('(20) expect createAd to call post function from requester with correct car data(manufacturer)', (done) => {
+
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + type;
+
+            let car = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+
+            let response = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+
+            requesterPostStub.returns(Promise.resolve(response));
+            data.createAd(car, authtoken, type)
+                .then(() => {
+                    let responseUsername = JSON.parse(requesterPostStub.args[0][1]).manufacturer;
+                    expect(responseUsername).to.equal(car.manufacturer);
+                })
+                .then(done, done);
+        })
+
+        it('(21) expect createAd to call post function from requester with correct car data(model)', (done) => {
+
+            let authtoken = 'authtoken123';
+            let type = 'cars';
+            let url = CONSTANTS.kinveyAppDataUrl + '/' + type;
+
+            let car = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+
+            let response = {
+                'manufacturer': "BMW",
+                'model': 'X5',
+                'year': '2006',
+                'kilometers': '80000',
+                'price': '20000',
+                'fuel': 'Diesel',
+                'transmission': 'Manual',
+                'horsepower': '200',
+                'type': 'type',
+                'numberOfDoors': '5',
+                'numberOfSeats': '5'
+            }
+
+            requesterPostStub.returns(Promise.resolve(response));
+            data.createAd(car, authtoken, type)
+                .then(() => {
+                    let responseUsername = JSON.parse(requesterPostStub.args[0][1]).model;
+                    expect(responseUsername).to.equal(car.model);
+                })
+                .then(done, done);
+        })
+    })
+    // add UserProfileImage
+
+    //TOD0 deleteVehicle 
 });
 
 
