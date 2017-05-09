48f81b5558b34c6de36c36fa454c7c0f58f153a6
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-05-09 14:29:57 +0300

    Added tests for loginUser and logoutUser functions

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -22,20 +22,19 @@ afterEach(clearSessionStorage);
 
 //data tests
 describe('Data Tests', () => {
-    describe('Data register new user', () => {
-        let sandbox;
+
+    // registerUser tests
+    describe('Data,js - registerUser', () => {
         let requesterPostStub;
         beforeEach(() => {
-            sandbox = sinon.sandbox.create();
             requesterPostStub = sinon.stub(requester, 'post');
         });
         afterEach(() => {
-            sandbox.restore();
             requesterPostStub.restore();
         });
 
         // with instance of new User
-        it('(1) expect register User to call post function from requester once', (done) => {
+        it('(1) expect registerUser to call post function from requester once', (done) => {
             requesterPostStub.returns(Promise.resolve());
             let userStub = sinon.createStubInstance(User);
             let password = '123456';
@@ -46,7 +45,7 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(2) expect register User to call post function with correct url', (done) => {
+        it('(2) expect registerUser to call post function with correct url', (done) => {
 
             requesterPostStub.returns(Promise.resolve());
             let url = CONSTANTS.kinveyRegisterUserUrl;
@@ -60,7 +59,7 @@ describe('Data Tests', () => {
         })
 
         // with  object user with property username
-        it('(3) expect register User to make a POST request to kinvey url', (done) => {
+        it('(3) expect registerUser to call post function from requester once', (done) => {
             requesterPostStub.returns(Promise.resolve());
 
             let user = {
@@ -75,7 +74,7 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(4) expect register User to call post function from requester once', (done) => {
+        it('(4) expect registerUser to make a POST request with correct url', (done) => {
 
             requesterPostStub.returns(Promise.resolve());
             let url = CONSTANTS.kinveyRegisterUserUrl;
@@ -91,7 +90,7 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(5) expect register User to call post function from requester with correct user data(username)', (done) => {
+        it('(5) expect registerUser to call post function from requester with correct user data(username)', (done) => {
 
             let url = CONSTANTS.kinveyRegisterUserUrl;
             let user = {
@@ -113,7 +112,7 @@ describe('Data Tests', () => {
                 .then(done, done);
         })
 
-        it('(6) expect register User to call post function from requester with correct user data(password)', (done) => {
+        it('(6) expect registerUser to call post function from requester with correct user data(password)', (done) => {
 
             let url = CONSTANTS.kinveyRegisterUserUrl;
             let user = {
@@ -137,8 +136,130 @@ describe('Data Tests', () => {
         })     
     })
 
+    // TODO login
+
+        describe('Data.js loginUser', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'post');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
+
+        it('(7) expect loginUser to call post function from requester once', (done) => {
+            let user = { 'username': "user", 'password': "123456" };
+            
+            requesterPostStub.returns(Promise.resolve());
+ 
+            data.loginUser(user)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
+
+        it('(8) expect loginUser to call post function with correct url', (done) => {
+
+            requesterPostStub.returns(Promise.resolve());
+            let url = CONSTANTS.kinveyRegisterUserUrl + '/login'
+            let user = { 'username': "user", 'password': "123456" };
+  
+            data.loginUser(user)
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        })
+
+        it('(9) expect loginUser to call post function from requester with correct user data(username)', (done) => {
+
+            requesterPostStub.returns(Promise.resolve());
+
+            let url = CONSTANTS.kinveyRegisterUserUrl + '/login'
+            let user = { 'username': "user", 'password': "123456" };
+
+            data.loginUser(user)
+                .then(() => {
+                    let responseUsername = JSON.parse(requesterPostStub.args[0][1]).username;
+                    expect(responseUsername).to.equal(user.username);
+                })
+                .then(done, done);
+        })
+
+        it('(10) expect loginUser to call post function from requester with correct user data(password)', (done) => {
+
+            requesterPostStub.returns(Promise.resolve());
+
+            let url = CONSTANTS.kinveyRegisterUserUrl + '/login'
+            let user = { 'username': "user", 'password': "123456" };
+
+            data.loginUser(user)
+                .then(() => {
+                    let responseUsername = JSON.parse(requesterPostStub.args[0][1]).password;
+                    expect(responseUsername).to.equal(user.password);
+                })
+                .then(done, done);
+        })     
+    })
+
+    //logout function
+
+    describe('Data.js logoutUser', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'post');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
+
+        it('(11) expect logoutUser to call post function from requester once', (done) => {
+            
+            requesterPostStub.returns(Promise.resolve());
+ 
+            data.logoutUser()
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.called.once;
+                })
+                .then(done, done);
+        })
+
+        it('(12) expect logoutUser to call post function with correct url', (done) => {
+            let url = CONSTANTS.kinveyRegisterUserUrl + '/_logout';
+            requesterPostStub.returns(Promise.resolve());
+  
+            data.logoutUser()
+                .then(() => {
+                    expect(requesterPostStub).to.have.been.calledWith(url);
+                })
+                .then(done, done);
+        }) 
+    })
 
 
+
+//TOD0 deleteVehicle 
+
+// TODO getVehicles
+
+//getMyAds
+// getUserProfileByAd
+
+//createAd
+
+// add UserProfileImage
+});
+
+
+
+
+
+// vehicle controller 
+describe('Vehicle controller tests', () => {
+    describe('Data register new user', () => {
+
+    });
 });
 
 
