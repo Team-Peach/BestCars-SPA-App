434f86d84b86a2ce170470c222c733d3b1162ffa
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-05-09 08:30:12 +0300

    Added unit test for registerUser function in data.js

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,21 +1,54 @@
 /* globals mocha, chai, describe, it  */
-
+import * as requester from 'requester';
 import * as data from 'data';
+import { User } from 'user';
 
 mocha.setup('bdd');
 let expect = chai.expect;
 
+const SESSION_STORAGE_USERNAME_KEY = 'username',
+    SESSION_STORAGE_AUTHTOKEN_KEY = 'authtoken',
+    SESSION_STORAGE_USERID_KEY = 'id';
+
+const clearSessionStorage = () => {
+    localStorage.removeItem(SESSION_STORAGE_USERNAME_KEY);
+    localStorage.removeItem(SESSION_STORAGE_AUTHTOKEN_KEY);
+    localStorage.removeItem(SESSION_STORAGE_USERID_KEY);
+};
+
+beforeEach(clearSessionStorage);
+afterEach(clearSessionStorage);
+
 //data tests
 describe('Data Tests', () => {
-    it('Test 1', () => {
+    describe('Data register new user', () => {
+        let requesterPostStub;
+        beforeEach(() => {
+            requesterPostStub = sinon.stub(requester, 'post');
+        });
+        afterEach(() => {
+            requesterPostStub.restore();
+        });
+
+        it('expect register User to call post function from requester once', (done) => {
+            requesterPostStub.returns(Promise.resolve());
+            let userStub = sinon.stub(User, 'constructor');
+            let password = '123456';
+            data.registerUser(userStub, password)
+            .then(() => {
+                expect(requesterPostStub).to.have.been.called.once;
+            })
+            .then(done, done);
+        })
+    })
+    /*
+    it('Test 1 - adForHome function should be invoke once with valid parameters', () => {
+        let vehicleType = 'cars';
+
     });
+    */
 });
 
-//user Controller test
-describe('User Controller Tests', () => {
-    it('Test 1', () => {
 
-    });
-});
 
 mocha.run();
